cf8cbe41f161f77c8fd42dc92e955b3b10d9a3b5
Azad Hüseyin Kılıç <73038601+economicseeker@users.noreply.github.com>
2025-06-09 17:04:15 +0300

    feat: Implement Tile component for displaying name and description data

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,9 +1,14 @@
 import React from "react";
 
-export const Tile = () => {
+export const Tile = ({ name, description }) => {
   return (
     <div className="tile-container">
-      
+      <p className="tile-title">{name}</p>
+      {Object.entries(description).map(([key, value]) => (
+        <p key={key} className="tile">
+          {value}
+        </p>
+      ))}
     </div>
   );
 };
